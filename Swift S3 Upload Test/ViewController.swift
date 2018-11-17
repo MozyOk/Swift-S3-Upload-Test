c6aee24bb2df977050899beb627027a01e448393
Masahito Okubo 'Mozy <mozy.okubo@gmail.com>
2018-11-18 01:32:37 +0900

    Initial Commit

---
+++ Swift S3 Upload Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift S3 Upload Test
+//
+//  Created by masahito-okubo on 2018/11/18.
+//  Copyright © 2018年 Mozy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
